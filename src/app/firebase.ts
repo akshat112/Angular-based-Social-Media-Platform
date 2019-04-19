755416eed9a68da7c1424fb772b79fb3864f4b23
Akshat Bhanchawat <akshat112@gmail.com>
2019-04-20 02:55:07 +0530

    basic setup done

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBZ9Ebp5Ng183SbK8D7NRrvU7kizea5kOs",
+    authDomain: "my-sm-project-3b324.firebaseapp.com",
+    databaseURL: "https://my-sm-project-3b324.firebaseio.com",
+    projectId: "my-sm-project-3b324",
+    storageBucket: "my-sm-project-3b324.appspot.com",
+    messagingSenderId: "149278754226"
+  };
+
+  export default config
